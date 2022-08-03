4516eff02e541422ca5da7eb535e678e7e156bc0
Kevin Web <keviinontiivero00@gmail.com>
2022-08-03 20:05:10 -0300

    agrego js

---
+++ miarchivo.js
@@ -0,0 +1,30 @@
+
+
+let confirmacion = confirm("¿Desea realizar una accion?")
+    if(confirmacion == true){
+        let pregunta = prompt("1) ¿Quiere ver un producto? \n 2) ¿Desea cotizar un precio?")
+        if(pregunta == "1"){
+            function Productos (nombre, precio){
+                this.nombre = nombre;
+                this.precio = precio;
+            };
+            var productos1 = new Productos("Lisoform", 320);
+            var productos2 = new Productos("Glade", 250);
+            var productos3 = new Productos("Bolsas", 250)
+            let producto = prompt("¿Que producto quiere visualizar? \n Lisoform, glade, bolsas, guantes, secadores, escobillones")
+            alert("Elegiste: " + producto)
+        }
+    
+        else if(pregunta == "2"){
+            let valorTotal = parseInt(prompt("Ingrese valor estimado: ")) ;
+            let cuotaPagar = prompt("Ahora escriba las cuotas a pagar (6, 12, 18, 24)");
+            while(cuotaPagar != 6 && cuotaPagar != 12 && cuotaPagar != 18 && cuotaPagar != 24){
+                cuotaPagar = prompt ("Ingrese un nro de cuotas valido (6, 12, 18, 24)");
+            }
+            let valorFinal = valorTotal / cuotaPagar;
+            alert ("Tus cuotas son: " + cuotaPagar + " cuotas, y el valor de cada cuota sera de: " + valorFinal)
+            }
+
+        }else{
+            alert("Usted seguira en la pagina")
+        }
